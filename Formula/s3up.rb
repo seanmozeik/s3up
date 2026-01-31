82683a41c0ded9cce840541cbe0c3f7587ff2fa4
Sean Lees <sean@Seans-MacBook-Air.local>
2026-01-31 21:53:49 +0000

    chore(s3up): bump formula to v0.3.0

--- Formula/s3up.rb
+++ Formula/s3up.rb
@@ -1,11 +1,12 @@
 class S3up < Formula
   desc "Fast CLI tool for uploading files to S3-compatible storage"
   homepage "https://github.com/seanmozeik/s3up"
-  version "0.2.2"
+  version "0.3.0"
   license "MIT"
 
+  # URL to bundled source (single JS file)
   url "https://github.com/seanmozeik/s3up/releases/download/v#{version}/s3up-#{version}.tar.gz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 "1a6d7f6d094963399f4274c98339a2debdcc6089508706e7fab537b2ea58d882"
 
   depends_on "oven-sh/bun/bun"
 
@@ -14,8 +15,14 @@ class S3up < Formula
   end
 
   def install
+    # Install all bundled files to libexec
     libexec.install Dir["*"]
-    (bin/"s3up").write_env_script "bun", libexec/"index.js"
+
+    # Create wrapper script
+    (bin/"s3up").write <<~EOS
+      #!/bin/bash
+      exec "#{Formula["bun"].opt_bin}/bun" "#{libexec}/index.js" "$@"
+    EOS
   end
 
   test do
